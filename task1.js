c05c290e97b38cab3360052a389a4a541eb1a8cf
Kristjan Krimm <kristjan.krimm@vikk.ee>
2021-09-08 14:51:51 +0300

    fifth and certainly last commit now

--- task1.js
+++ task1.js
@@ -1,7 +1,7 @@
 let a = 5;
 let b = 10;
 let c = a + b;
-console.log(c); //liidab 5+10=15 
+console.log(c); //adds 5+10
 a = "5";
 let d = a + b;
 console.log(d); //liidab küll kokku aga mitte matemaatiliselt vaid väärtusena, ehk siis 5 liidetakse 10 ette, ehk siis vastus=510
