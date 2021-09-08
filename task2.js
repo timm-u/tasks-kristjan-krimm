de94259a3d81836b201960b35507feb28d68df21
Kristjan Krimm <kristjan.krimm@vikk.ee>
2021-09-08 15:58:01 +0300

    combined task2 and task2_1 to one file

--- task2.js
+++ task2.js
@@ -5,3 +5,9 @@ const synnaaasta = ' 1998';''
 let hobid = [ 'klaverimäng', 'elektroonika' ];
 const kokku = {eesnimi, synnapaev, synnakuu, synnaaasta, hobid};
 console.log(synnapaev,synnakuu,synnaaasta);
+
+const eesnimi = 'Kristjan';
+const synna = new Date(1998, 11, 22, 0, 0, 0, 0);
+let hobid = [ 'klaverimäng', 'elektroonika' ];
+const kokku = {eesnimi, synna, hobid};
+console.log(synna.getDate(),".",synna.getMonth()+1,".",synna.getFullYear());
--- task2_1.js
+++
@@ -1,5 +0,0 @@
-const eesnimi = 'Kristjan';
-const synna = new Date(1998, 11, 22, 0, 0, 0, 0);
-let hobid = [ 'klaverimäng', 'elektroonika' ];
-const kokku = {eesnimi, synna, hobid};
-console.log(synna.getDate(),".",synna.getMonth()+1,".",synna.getFullYear());
