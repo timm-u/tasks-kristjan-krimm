9e9ae16cfa4b74b5fd224a7b33bf528360d7aac5
= <=>
2021-09-08 12:28:05 +0300

    first commit

---
+++ task2.js
@@ -0,0 +1,7 @@
+const eesnimi = 'Kristjan';
+const synnapaev = '22';
+const synnakuu = 'detsember';
+const synnaaasta = ' 1998';''
+let hobid = [ 'klaverimäng', 'elektroonika' ];
+const kokku = {eesnimi, synnapaev, synnakuu, synnaaasta, hobid};
+console.log(synnapaev,synnakuu,synnaaasta);
---
+++ task3.js
@@ -0,0 +1,18 @@
+let name;
+
+function greet(name){
+    if(name === undefined){
+        return ('Hello, my friend.');
+    } else if(name instanceof Array) {
+        if(name.length >= 1){
+                return ("Hello, " + name[0] + " and " + name[1]);
+        }
+    }
+    else{
+        return ("Hello,", name);
+    }
+}
+
+console.log(greet('Kristjan'));
+
+console.log(greet(['Bob','June']));
