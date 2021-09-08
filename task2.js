e0aeedd140204569a506c5b543a9b1b8017cf862
Kristjan Krimm <kristjan.krimm@vikk.ee>
2021-09-08 15:59:42 +0300

    must make sure that the code actually works

--- task2.js
+++ task2.js
@@ -6,8 +6,8 @@ let hobid = [ 'klaverimäng', 'elektroonika' ];
 const kokku = {eesnimi, synnapaev, synnakuu, synnaaasta, hobid};
 console.log(synnapaev,synnakuu,synnaaasta);
 
-const eesnimi = 'Kristjan';
+const eesnimi1 = 'Kristjan';
 const synna = new Date(1998, 11, 22, 0, 0, 0, 0);
-let hobid = [ 'klaverimäng', 'elektroonika' ];
-const kokku = {eesnimi, synna, hobid};
+let hobid2 = [ 'klaverimäng', 'elektroonika' ];
+const kokku2 = {eesnimi1, synna, hobid2};
 console.log(synna.getDate(),".",synna.getMonth()+1,".",synna.getFullYear());
